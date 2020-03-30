79a6bdf511cc5796c07e05ff12fec6c03dfeb68d
ShinyLux <Sv647340!>
2020-03-30 14:54:15 -0600

    Adding typecheck

--- Typecheck.java
+++ Typecheck.java
@@ -1,11 +1,35 @@
-import syntaxtree.Goal;
-import myVisitor.PrettyPrinterVisitor;
+import java.io.*;
+import visitor.*;
+import syntaxtree.*;
+import java.util.*;
+import picojava.*;
 
-public class Typecheck{
-	public static void main(String args[]) throws ParseException{
-		MiniJavaParser parser = new MiniJavaParser(System.in);
-		Goal tee = parser.Goal();
-		PrettyPrinterVisitor printer = new PrettyPrinterVisitor();
-		printer.visit(tree);	
+public class Typecheck {
+	public static void main(String[] args) {
+		Node root = null;
+		try {
+			root = new MiniJavaParser(System.in).Goal();
+
+			// Pretty-print the tree.
+			PPrinter<Void,String> pp = new PPrinter<Void,String>();
+			root.accept(pp, "");
+
+			// Build the symbol table.
+			SymTableVis<Void, Integer> pv =
+					new SymTableVis<Void,Integer>();
+			root.accept(pv, 0);
+			HashMap<String, String> symt = pv.symt;
+			// Do type checking.
+			TypeCheckSimp ts = new TypeCheckSimp();
+			MyType res = root.accept(ts, symt);
+			if (res != null && res.type_array.size() > 0)
+				System.out.println("Code typechecks");
+			else
+				System.out.println("Type error");
+		}
+		catch (ParseException e) {
+			System.out.println(e.toString());
+			System.exit(1);
+		}
 	}
 }
